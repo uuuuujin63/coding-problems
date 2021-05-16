c49c63a563165ce03b1cc222cd50294de995b28e
uuuuujin63 <yjkim012279@gmail.com>
2021-05-16 10:45:38 +0900

    5/16 commit

--- programmers/1week/폰켓몬.py
+++ programmers/1week/폰켓몬.py
@@ -1,5 +1,10 @@
 def solution(nums):
     answer = 0
+    s_nums = set(nums)
+    if (len(s_nums)>=(len(nums)//2)):
+        answer = len(nums)//2
+    else:
+        answer = len(s_nums)
     return answer
 
 nums = [3,1,2,3]
