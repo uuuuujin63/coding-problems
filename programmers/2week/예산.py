3550a0567a827033e2bb9f5d0909690a599e4e51
uuuuujin63 <yjkim012279@gmail.com>
2021-05-22 00:11:28 +0900

    5/22 commit

--- programmers/2week/예산.py
+++ programmers/2week/예산.py
@@ -1,3 +1,12 @@
 def solution(d, budget):
     answer = 0
+    d.sort()
+    for i in range(len(d)):
+        if sum(d[:i+1])<=budget:
+            answer += 1
     return answer
+
+d = [1,3,2,5,4]
+budget = 9
+
+print(solution(d, budget))
