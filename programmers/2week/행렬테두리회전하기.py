56312f769674e3c9b4086446b4730e02d519dfdf
uuuuujin63 <yjkim012279@gmail.com>
2021-05-26 17:34:46 +0900

    5/26 commit

--- programmers/2week/행렬테두리회전하기.py
+++ programmers/2week/행렬테두리회전하기.py
@@ -3,30 +3,35 @@ def solution(rows, columns, queries):
     arr = []
     for i in range(1, rows+1):
         arr.append(list(columns*(i-1)+j for j in range(1, columns+1)))
-    for x1, y1, x2, y2 in queries:
-        x1 -= 1
+    for y1, x1, y2, x2 in queries:
         y1 -= 1
-        x2 -= 1
+        x1 -= 1
         y2 -= 1
-        for i in range(rows):
-            print(arr[i])
+        x2 -= 1
+       
         minn = rows*columns
-        tmp = arr[y1][x1]
-        for nx in range(x1, x2):
-            arr[y1][nx] = arr[y1][nx+1]
-            minn = min(arr[y1][nx], minn)
-        for ny in range(y1, y2):
-            arr[ny][x2] = arr[ny+1][x2]
-            minn = min(arr[ny][x2], minn)
+        y1x2 = arr[y1][x2]
         for nx in range(x2, x1, -1):
-            arr[y2][nx] = arr[y1][nx-1]
+            arr[y1][nx] = arr[y1][nx-1]
+            minn = min(arr[y1][nx], minn)
+        
+        y2x2 = arr[y2][x2]
+        for ny in range(y2, y1+1, -1):
+            arr[ny][x2] = arr[ny-1][x2]
+            minn = min(arr[ny][x2], minn)
+        arr[y1+1][x2] = y1x2
+        
+        y2x1 = arr[y2][x1]
+        for nx in range(x1, x2):
+            arr[y2][nx] = arr[y2][nx+1]
             minn = min(arr[y2][nx], minn)
-        for ny in range(y2, y1, -1):
-            arr[ny][x1] = arr[ny-1][x1]
+        arr[y2][x2-1] = y2x2
+        
+        for ny in range(y1, y2):
+            arr[ny][x1] = arr[ny+1][x1]
             minn = min(arr[ny][x1], minn)
-        arr[ny][x1+1] = tmp
-        for i in range(rows):
-            print(arr[i])
+        arr[y2-1][x1] = y2x1
+        
         answer.append(minn)
     return answer
 
