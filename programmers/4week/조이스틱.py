a1a58d41c0a001e6350b41596dea4934eb6d8e13
uuuuujin63 <yjkim012279@gmail.com>
2021-06-08 23:16:56 +0900

    6/8 commit

--- programmers/4week/조이스틱.py
+++ programmers/4week/조이스틱.py
@@ -1,3 +1,14 @@
 def solution(name):
     answer = 0
+    name = list(name)
+    base = ["A"]*len(name)
+    idx = 0
+    while 1:
+        r = 1
+        l = 1
+        if name[idx]!="A":
+            
     return answer
+
+name = "JEROEN"
+print(solution(name))
