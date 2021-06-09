0d343868f6333ed383655be629383c1504ad5dad
uuuuujin63 <yjkim012279@gmail.com>
2021-06-09 13:02:25 +0900

    6/9 commit

--- programmers/4week/조이스틱.py
+++ programmers/4week/조이스틱.py
@@ -7,7 +7,26 @@ def solution(name):
         r = 1
         l = 1
         if name[idx]!="A":
-            
+            answer += min(ord("Z")-ord(name[idx])+1, ord(name[idx])-ord("A"))
+            name[idx] = "A"
+        if name == base:
+            break
+        else:
+            for i in range(1, len(name)):
+                if name[idx+i] == "A":
+                    r += 1
+                else:
+                    break
+                if name[idx-i] == "A":
+                    l += 1
+                else:
+                    break
+            if r>l:
+                answer += l
+                idx -=l
+            else:
+                answer += r
+                idx += r
     return answer
 
 name = "JEROEN"
