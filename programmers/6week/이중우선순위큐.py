25ddb8681acc6a452fec3fb9f620db006e98f349
uuuuujin63 <yjkim012279@gmail.com>
2021-06-20 01:00:07 +0900

    6/20 commit

--- programmers/6week/이중우선순위큐.py
+++ programmers/6week/이중우선순위큐.py
@@ -1,3 +1,22 @@
+import heapq
 def solution(operations):
-    answer = []
-    return answer
+    hq = []
+    for oper in operations:
+        num = int(oper[2:])
+        if oper[0] =='I':
+            heapq.heappush(hq, num)
+        else:
+            if len(hq) == 0:
+                continue
+            elif num == 1:
+                hq = heapq.nlargest(len(hq), hq)[1:]
+                heapq.heapify(hq)
+            else:
+                print(heapq.heappop(hq))
+        print(hq)
+    if len(hq)==0:
+        return [0, 0]
+    else:
+        return [heapq.nlargest(1, hq)[0], heapq.nsmallest(1, hq)[0]]
+#print(solution(["I 7","I 5","I -5","D -1"]))
+print(solution(["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]))
