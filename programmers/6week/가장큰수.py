6339c32114ead1a064ee891ac659fe0338d04e7e
uuuuujin63 <yjkim012279@gmail.com>
2021-06-22 17:00:14 +0900

    solved 가장큰수

--- programmers/6week/가장큰수.py
+++ programmers/6week/가장큰수.py
@@ -1,3 +1,8 @@
 def solution(numbers):
     answer = ''
+    n = list(map(str, numbers))
+    n.sort(key=lambda x:x*3, reverse=True)
+    answer = str(int(''.join(n)))
     return answer
+
+print(solution([6, 10, 2]))
