b4c55326028a72af80fa985c56c6b72bc1736ba1
uuuuujin63 <yjkim012279@gmail.com>
2021-07-06 23:11:47 +0900

    solved 타겟넘버.py

--- programmers/8week/타겟넘버.py
+++ programmers/8week/타겟넘버.py
@@ -1,3 +1,14 @@
+from collections import deque
 def solution(numbers, target):
     answer = 0
+    s = deque()
+    s.append(0)
+    for number in numbers:
+        for _ in range(len(s)):
+            n = s.popleft()
+            s.append((n+number))
+            s.append((n-number))
+    answer = s.count(target)
     return answer
+
+print(solution([1, 1, 1, 1, 1], 3))
