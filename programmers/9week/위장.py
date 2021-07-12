42bf2c94f542390240fbd74fab1564469bc29c94
uuuuujin63 <yjkim012279@gmail.com>
2021-07-12 09:28:22 +0900

    solved 위장.py

--- programmers/9week/위장.py
+++ programmers/9week/위장.py
@@ -1,3 +1,11 @@
 def solution(clothes):
-    answer = 0
-    return answer
+    answer = 1
+    dic = {}
+    for name, c in clothes:
+        if c in dic.keys():
+            dic[c] += 1
+        else:
+            dic[c] = 2
+    for n in dic.values():
+        answer *= n
+    return answer-1
