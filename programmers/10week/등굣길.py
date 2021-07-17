08a4506bb17f7fbfe5fb67ab1a068ec5d3d00c94
uuuuujin63 <yjkim012279@gmail.com>
2021-07-17 20:31:57 +0900

    solved 등굣길.py

--- programmers/10week/등굣길.py
+++ programmers/10week/등굣길.py
@@ -1,6 +1,16 @@
 def solution(m, n, puddles):
     answer = 0
-    dp = [[1 for _ in range(m)] for _ in range(n)]
-    return answer
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    dp[1][1] = 1
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i==1 and j==1:
+                continue
+            elif [j, i] in puddles:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+    answer = dp[-1][-1]
+    return answer%1000000007
 
 print(solution(4,3,[[2,2]]))
